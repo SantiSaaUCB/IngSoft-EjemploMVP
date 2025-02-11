d0d66724952d6abb1b166da946673364bccabafd
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2025-02-11 10:32:03 -0400

    Funcion Multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,29 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const firstS = document.querySelector("#primer-numeroS");
+const secondS = document.querySelector("#segundo-numeroS");
+const firstM = document.querySelector("#primer-numeroM");
+const secondM = document.querySelector("#segundo-numeroM");
+const formS = document.querySelector("#sumar-form");
+const formM = document.querySelector('#multiplicar-form')
+const divS = document.querySelector("#resultado-divS");
+const divM = document.querySelector("#resultado-divM")
 
-form.addEventListener("submit", (event) => {
+formS.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(firstS.value);
+  const secondNumber = Number.parseInt(secondS.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  divS.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formM.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstM.value);
+  const secondNumber = Number.parseInt(secondM.value);
+
+  divM.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
